241bb3912a34993cbc77e43155c069f5b9fead4c
Annette <annette.ngugi@student.moringaschool.com>
2025-05-15 11:11:47 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        result = []
+        original = sorted(self.word.lower())
+
+        for word in word_list:
+            if sorted(word.lower()) == original:
+                result.append(word)
+        return result
